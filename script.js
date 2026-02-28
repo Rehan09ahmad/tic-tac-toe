de0c80653149fbb5c3c038b39aee59a60242bfc9
rehan <rehanahmad3775@gmail.com>
2026-02-28 15:39:13 +0500

    feat: complete Tic Tac Toe game with winner and draw logic

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
 let boxes = document.querySelectorAll(".box");
-let restetbtn = document.querySelector("#rest-btn");
+let resetBtn = document.querySelector("#reset-btn");
+let newGameBtn = document.querySelector("#new-btn");
+let msgContainer = document.querySelector(".msg-container");
+let msg = document.querySelector("#msg");
 
-let turn0 = true;// PlayrX player0
+let turnO = true; // Player O starts
+let count = 0; // move counter
 
-const winopatterns =[
+const winPatterns = [
     [0, 1, 2],
     [0, 3, 6],
     [0, 4, 8],
@@ -13,19 +17,83 @@ const winopatterns =[
     [3, 4, 5],
     [6, 7, 8],
 ];
- 
-boxes.forEach((box) => { 
+
+// Reset Game
+const resetGame = () => {
+    turnO = true;
+    count = 0;
+    enableBoxes();
+    msgContainer.classList.add("hide");
+};
+
+// Box Click
+boxes.forEach((box) => {
     box.addEventListener("click", () => {
-        console.log("box was click");
-        if(turn0) {
-            //player0
-            box.innerText = "0";
-            turn0 = false;
-        }else{
-            //playerX
+        if (turnO) {
+            box.innerText = "O";
+            turnO = false;
+        } else {
             box.innerText = "X";
-            turn0 = true;
+            turnO = true;
         }
+
         box.disabled = true;
+        count++;
+
+        checkWinner();
     });
 });
+
+// Disable Boxes
+const disableBoxes = () => {
+    boxes.forEach((box) => {
+        box.disabled = true;
+    });
+};
+
+// Enable Boxes
+const enableBoxes = () => {
+    boxes.forEach((box) => {
+        box.disabled = false;
+        box.innerText = "";
+    });
+};
+
+// Show Winner
+const showWinner = (winner) => {
+    msg.innerText = `🎉 Congratulations! Winner is ${winner}`;
+    msgContainer.classList.remove("hide");
+    disableBoxes();
+};
+
+// Show Draw
+const showDraw = () => {
+    msg.innerText = "😮 It's a Draw!";
+    msgContainer.classList.remove("hide");
+    disableBoxes();
+};
+
+// Check Winner
+const checkWinner = () => {
+    for (let pattern of winPatterns) {
+        let pos1 = boxes[pattern[0]].innerText;
+        let pos2 = boxes[pattern[1]].innerText;
+        let pos3 = boxes[pattern[2]].innerText;
+
+        if (pos1 !== "" && pos2 !== "" && pos3 !== "") {
+            if (pos1 === pos2 && pos2 === pos3) {
+                showWinner(pos1);
+                return;
+            }
+        }
+    }
+
+    // Draw Condition
+    if (count === 9) {
+        showDraw();
+    }
+};
+
+// Buttons
+newGameBtn.addEventListener("click", resetGame);
+resetBtn.addEventListener("click", resetGame);
