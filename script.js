3833ca28ee80ea183fd973cbb747c3fe64028d90
rehan <rehanahmad3775@gmail.com>
2026-02-28 11:18:32 +0500

    Creating tic tac toe game

---
+++ script.js
@@ -0,0 +1,31 @@
+let boxes = document.querySelectorAll(".box");
+let restetbtn = document.querySelector("#rest-btn");
+
+let turn0 = true;// PlayrX player0
+
+const winopatterns =[
+    [0, 1, 2],
+    [0, 3, 6],
+    [0, 4, 8],
+    [1, 4, 7],
+    [2, 5, 8],
+    [2, 4, 6],
+    [3, 4, 5],
+    [6, 7, 8],
+];
+ 
+boxes.forEach((box) => { 
+    box.addEventListener("click", () => {
+        console.log("box was click");
+        if(turn0) {
+            //player0
+            box.innerText = "0";
+            turn0 = false;
+        }else{
+            //playerX
+            box.innerText = "X";
+            turn0 = true;
+        }
+        box.disabled = true;
+    });
+});
